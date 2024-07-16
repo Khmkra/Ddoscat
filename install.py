4e08642ab2ef5b0cdf5489c985a2e50c97cc6c75
Khmkra <153583693+Khmkra@users.noreply.github.com>
2024-07-16 22:26:37 +0700

    Update install.py

--- install.py
+++ install.py
@@ -1,15 +1,15 @@
 import os
 import sys
 
-os.system('pkg update -y && pkg install wget curl proot tar -y && wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Debian/debian.sh -O debian.sh && chmod +x debian.sh && bash debian.sh')
+os.system('apt update -y && pkg install wget curl proot tar -y && wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Debian/debian.sh -O debian.sh && chmod +x debian.sh && bash debian.sh')
 os.system('apt install npm')
 os.system('apt-get install nodejs -y')
 os.system('apt-get install perl -y')
-os.system('pkg install openjdk-17')
-os.system('pkg install nodejs-lts')
-os.system('pkg install -y tur-repo') 
-os.system('pkg install -y code-server code-server --version')
-os.system('pkg install qb64')
+os.system('apt install openjdk-17')
+os.system('apt install nodejs-lts')
+os.system('apt install -y tur-repo') 
+os.system('apt install -y code-server code-server --version')
+os.system('apt install qb64')
 os.system('npm install url')
 os.system('npm install https')
 os.system('npm i requests')
